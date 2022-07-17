cc1e92c83e5faeae30c03ff4ad8843ea1123c858
barbosaleo98 <leonardobarbosa_98@hotmail.com>
2022-07-17 14:25:18 -0300

    Created initial dir for V2X simulation

---
+++ V2X_Politecnico/out/gcc-debug/src/veins_inet/VeinsInetApplicationBase.o.d
@@ -0,0 +1,284 @@
+../out/gcc-debug/src/veins_inet/VeinsInetApplicationBase.o: \
+ veins_inet/VeinsInetApplicationBase.cc \
+ veins_inet/VeinsInetApplicationBase.h veins_inet/veins_inet.h \
+ /home/leonardo/artery/extern/veins/src/veins/veins.h \
+ /home/leonardo/omnetpp-5.6.2/include/omnetpp.h \
+ ../../inet/src/inet/common/INETDefs.h \
+ ../../inet/src/inet/common/precompiled_debug.h \
+ ../../inet/src/inet/common/precompiled.h \
+ ../../inet/src/inet/opp_defines.h ../../inet/src/inet/features.h \
+ ../../inet/src/inet/common/Compat.h \
+ ../../inet/src/inet/common/InitStages.h \
+ ../../inet/src/inet/applications/base/ApplicationBase.h \
+ ../../inet/src/inet/common/lifecycle/ModuleOperations.h \
+ ../../inet/src/inet/common/lifecycle/LifecycleOperation.h \
+ ../../inet/src/inet/common/lifecycle/OperationalBase.h \
+ ../../inet/src/inet/common/lifecycle/ILifecycle.h \
+ ../../inet/src/inet/transportlayer/contract/udp/UdpSocket.h \
+ ../../inet/src/inet/common/packet/Message.h \
+ ../../inet/src/inet/common/packet/tag/TagSet.h \
+ ../../inet/src/inet/common/packet/Packet.h \
+ ../../inet/src/inet/common/packet/chunk/BitsChunk.h \
+ ../../inet/src/inet/common/packet/chunk/Chunk.h \
+ ../../inet/src/inet/common/MemoryInputStream.h \
+ ../../inet/src/inet/common/Units.h ../../inet/src/inet/common/INETMath.h \
+ ../../inet/src/inet/linklayer/common/MacAddress.h \
+ ../../inet/src/inet/networklayer/contract/ipv4/Ipv4Address.h \
+ ../../inet/src/inet/networklayer/contract/ipv6/Ipv6Address.h \
+ ../../inet/src/inet/common/MemoryOutputStream.h \
+ ../../inet/src/inet/common/Ptr.h \
+ ../../inet/src/inet/common/IntrusivePtr.h \
+ ../../inet/src/inet/common/packet/tag/RegionTagSet.h \
+ ../../inet/src/inet/common/TemporarySharedPtr.h \
+ ../../inet/src/inet/common/packet/chunk/BytesChunk.h \
+ ../../inet/src/inet/common/socket/ISocket.h \
+ ../../inet/src/inet/networklayer/common/L3Address.h \
+ ../../inet/src/inet/networklayer/common/ModuleIdAddress.h \
+ ../../inet/src/inet/networklayer/common/ModulePathAddress.h \
+ ../../inet/src/inet/networklayer/contract/clns/ClnsAddress.h \
+ ../../inet/src/inet/networklayer/contract/IInterfaceTable.h \
+ ../../inet/src/inet/networklayer/common/InterfaceEntry.h \
+ ../../inet/src/inet/common/Simsignals.h \
+ ../../inet/src/inet/common/Simsignals_m.h \
+ ../../inet/src/inet/common/INETDefs_m.h \
+ ../../inet/src/inet/networklayer/common/InterfaceToken.h \
+ ../../inet/src/inet/transportlayer/contract/udp/UdpControlInfo.h \
+ ../../inet/src/inet/transportlayer/contract/udp/UdpControlInfo_m.h \
+ ../../inet/src/inet/networklayer/common/IpProtocolId_m.h \
+ ../../inet/src/inet/networklayer/common/L3Address_m.h \
+ veins_inet/VeinsInetMobility.h \
+ ../../inet/src/inet/mobility/base/MobilityBase.h \
+ ../../inet/src/inet/common/ModuleAccess.h \
+ ../../inet/src/inet/common/StringFormat.h \
+ ../../inet/src/inet/common/geometry/common/CanvasProjection.h \
+ ../../inet/src/inet/common/geometry/common/RotationMatrix.h \
+ ../../inet/src/inet/common/geometry/common/Coord.h \
+ ../../inet/src/inet/common/geometry/common/EulerAngles.h \
+ ../../inet/src/inet/common/geometry/common/Quaternion.h \
+ ../../inet/src/inet/mobility/contract/IMobility.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIScenarioManager.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/utils/Coord.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/utils/FWMath.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/modules/BaseWorldUtility.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/connectionManager/BaseConnectionManager.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/utils/AntennaPosition.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/connectionManager/NicEntry.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/utils/Heading.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/utility/HasLogProxy.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/utils/FindModule.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/obstacle/ObstacleControl.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/obstacle/Obstacle.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/world/annotations/AnnotationManager.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/utility/BBoxLookup.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/obstacle/VehicleObstacleControl.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/utils/Move.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/obstacle/MobileHostObstacle.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIBuffer.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIConstants.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIColor.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIConnection.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCICoord.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/VehicleSignal.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/utils/EnumBitset.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCICommandInterface.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/utility/TimerManager.h \
+ ../../inet/src/inet/common/TagBase_m.h \
+ ../../inet/src/inet/common/TimeTag_m.h \
+ ../../inet/src/inet/common/Units_m.h \
+ ../../inet/src/inet/networklayer/common/L3AddressResolver.h \
+ ../../inet/src/inet/networklayer/contract/IRoutingTable.h \
+ ../../inet/src/inet/networklayer/contract/IRoute.h \
+ ../../inet/src/inet/networklayer/common/L3AddressTag_m.h
+
+veins_inet/VeinsInetApplicationBase.h:
+
+veins_inet/veins_inet.h:
+
+/home/leonardo/artery/extern/veins/src/veins/veins.h:
+
+/home/leonardo/omnetpp-5.6.2/include/omnetpp.h:
+
+../../inet/src/inet/common/INETDefs.h:
+
+../../inet/src/inet/common/precompiled_debug.h:
+
+../../inet/src/inet/common/precompiled.h:
+
+../../inet/src/inet/opp_defines.h:
+
+../../inet/src/inet/features.h:
+
+../../inet/src/inet/common/Compat.h:
+
+../../inet/src/inet/common/InitStages.h:
+
+../../inet/src/inet/applications/base/ApplicationBase.h:
+
+../../inet/src/inet/common/lifecycle/ModuleOperations.h:
+
+../../inet/src/inet/common/lifecycle/LifecycleOperation.h:
+
+../../inet/src/inet/common/lifecycle/OperationalBase.h:
+
+../../inet/src/inet/common/lifecycle/ILifecycle.h:
+
+../../inet/src/inet/transportlayer/contract/udp/UdpSocket.h:
+
+../../inet/src/inet/common/packet/Message.h:
+
+../../inet/src/inet/common/packet/tag/TagSet.h:
+
+../../inet/src/inet/common/packet/Packet.h:
+
+../../inet/src/inet/common/packet/chunk/BitsChunk.h:
+
+../../inet/src/inet/common/packet/chunk/Chunk.h:
+
+../../inet/src/inet/common/MemoryInputStream.h:
+
+../../inet/src/inet/common/Units.h:
+
+../../inet/src/inet/common/INETMath.h:
+
+../../inet/src/inet/linklayer/common/MacAddress.h:
+
+../../inet/src/inet/networklayer/contract/ipv4/Ipv4Address.h:
+
+../../inet/src/inet/networklayer/contract/ipv6/Ipv6Address.h:
+
+../../inet/src/inet/common/MemoryOutputStream.h:
+
+../../inet/src/inet/common/Ptr.h:
+
+../../inet/src/inet/common/IntrusivePtr.h:
+
+../../inet/src/inet/common/packet/tag/RegionTagSet.h:
+
+../../inet/src/inet/common/TemporarySharedPtr.h:
+
+../../inet/src/inet/common/packet/chunk/BytesChunk.h:
+
+../../inet/src/inet/common/socket/ISocket.h:
+
+../../inet/src/inet/networklayer/common/L3Address.h:
+
+../../inet/src/inet/networklayer/common/ModuleIdAddress.h:
+
+../../inet/src/inet/networklayer/common/ModulePathAddress.h:
+
+../../inet/src/inet/networklayer/contract/clns/ClnsAddress.h:
+
+../../inet/src/inet/networklayer/contract/IInterfaceTable.h:
+
+../../inet/src/inet/networklayer/common/InterfaceEntry.h:
+
+../../inet/src/inet/common/Simsignals.h:
+
+../../inet/src/inet/common/Simsignals_m.h:
+
+../../inet/src/inet/common/INETDefs_m.h:
+
+../../inet/src/inet/networklayer/common/InterfaceToken.h:
+
+../../inet/src/inet/transportlayer/contract/udp/UdpControlInfo.h:
+
+../../inet/src/inet/transportlayer/contract/udp/UdpControlInfo_m.h:
+
+../../inet/src/inet/networklayer/common/IpProtocolId_m.h:
+
+../../inet/src/inet/networklayer/common/L3Address_m.h:
+
+veins_inet/VeinsInetMobility.h:
+
+../../inet/src/inet/mobility/base/MobilityBase.h:
+
+../../inet/src/inet/common/ModuleAccess.h:
+
+../../inet/src/inet/common/StringFormat.h:
+
+../../inet/src/inet/common/geometry/common/CanvasProjection.h:
+
+../../inet/src/inet/common/geometry/common/RotationMatrix.h:
+
+../../inet/src/inet/common/geometry/common/Coord.h:
+
+../../inet/src/inet/common/geometry/common/EulerAngles.h:
+
+../../inet/src/inet/common/geometry/common/Quaternion.h:
+
+../../inet/src/inet/mobility/contract/IMobility.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIScenarioManager.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/utils/Coord.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/utils/FWMath.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/modules/BaseWorldUtility.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/connectionManager/BaseConnectionManager.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/utils/AntennaPosition.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/connectionManager/NicEntry.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/utils/Heading.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/utility/HasLogProxy.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/utils/FindModule.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/obstacle/ObstacleControl.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/obstacle/Obstacle.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/world/annotations/AnnotationManager.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/utility/BBoxLookup.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/obstacle/VehicleObstacleControl.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/utils/Move.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/obstacle/MobileHostObstacle.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIBuffer.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIConstants.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIColor.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIConnection.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCICoord.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/VehicleSignal.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/utils/EnumBitset.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCICommandInterface.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/utility/TimerManager.h:
+
+../../inet/src/inet/common/TagBase_m.h:
+
+../../inet/src/inet/common/TimeTag_m.h:
+
+../../inet/src/inet/common/Units_m.h:
+
+../../inet/src/inet/networklayer/common/L3AddressResolver.h:
+
+../../inet/src/inet/networklayer/contract/IRoutingTable.h:
+
+../../inet/src/inet/networklayer/contract/IRoute.h:
+
+../../inet/src/inet/networklayer/common/L3AddressTag_m.h:
---
+++ V2X_Politecnico/out/gcc-release/src/veins_inet/VeinsInetMobility.o.d
@@ -0,0 +1,195 @@
+../out/gcc-release/src/veins_inet/VeinsInetMobility.o: \
+ veins_inet/VeinsInetMobility.cc veins_inet/VeinsInetMobility.h \
+ ../../inet/src/inet/mobility/base/MobilityBase.h \
+ ../../inet/src/inet/common/ModuleAccess.h \
+ ../../inet/src/inet/common/INETDefs.h \
+ ../../inet/src/inet/common/precompiled_release.h \
+ ../../inet/src/inet/common/precompiled.h \
+ /home/leonardo/omnetpp-5.6.2/include/omnetpp.h \
+ ../../inet/src/inet/opp_defines.h ../../inet/src/inet/features.h \
+ ../../inet/src/inet/common/Compat.h \
+ ../../inet/src/inet/common/InitStages.h \
+ ../../inet/src/inet/networklayer/contract/IInterfaceTable.h \
+ ../../inet/src/inet/networklayer/common/InterfaceEntry.h \
+ ../../inet/src/inet/common/packet/tag/TagSet.h \
+ ../../inet/src/inet/common/Simsignals.h \
+ ../../inet/src/inet/common/Simsignals_m.h \
+ ../../inet/src/inet/common/INETDefs_m.h \
+ ../../inet/src/inet/linklayer/common/MacAddress.h \
+ ../../inet/src/inet/networklayer/common/InterfaceToken.h \
+ ../../inet/src/inet/networklayer/common/L3Address.h \
+ ../../inet/src/inet/networklayer/common/ModuleIdAddress.h \
+ ../../inet/src/inet/networklayer/common/ModulePathAddress.h \
+ ../../inet/src/inet/networklayer/contract/clns/ClnsAddress.h \
+ ../../inet/src/inet/networklayer/contract/ipv4/Ipv4Address.h \
+ ../../inet/src/inet/networklayer/contract/ipv6/Ipv6Address.h \
+ ../../inet/src/inet/common/StringFormat.h \
+ ../../inet/src/inet/common/geometry/common/CanvasProjection.h \
+ ../../inet/src/inet/common/geometry/common/RotationMatrix.h \
+ ../../inet/src/inet/common/geometry/common/Coord.h \
+ ../../inet/src/inet/common/INETMath.h \
+ ../../inet/src/inet/common/geometry/common/EulerAngles.h \
+ ../../inet/src/inet/common/Units.h \
+ ../../inet/src/inet/common/geometry/common/Quaternion.h \
+ ../../inet/src/inet/mobility/contract/IMobility.h \
+ veins_inet/veins_inet.h \
+ /home/leonardo/artery/extern/veins/src/veins/veins.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIScenarioManager.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/utils/Coord.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/utils/FWMath.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/modules/BaseWorldUtility.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/connectionManager/BaseConnectionManager.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/utils/AntennaPosition.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/connectionManager/NicEntry.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/utils/Heading.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/utility/HasLogProxy.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/utils/FindModule.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/obstacle/ObstacleControl.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/obstacle/Obstacle.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/world/annotations/AnnotationManager.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/utility/BBoxLookup.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/obstacle/VehicleObstacleControl.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/utils/Move.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/obstacle/MobileHostObstacle.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIBuffer.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIConstants.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIColor.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIConnection.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCICoord.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/VehicleSignal.h \
+ /home/leonardo/artery/extern/veins/src/veins/base/utils/EnumBitset.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCICommandInterface.h \
+ /home/leonardo/artery/extern/veins/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h \
+ ../../inet/src/inet/common/geometry/common/GeographicCoordinateSystem.h
+
+veins_inet/VeinsInetMobility.h:
+
+../../inet/src/inet/mobility/base/MobilityBase.h:
+
+../../inet/src/inet/common/ModuleAccess.h:
+
+../../inet/src/inet/common/INETDefs.h:
+
+../../inet/src/inet/common/precompiled_release.h:
+
+../../inet/src/inet/common/precompiled.h:
+
+/home/leonardo/omnetpp-5.6.2/include/omnetpp.h:
+
+../../inet/src/inet/opp_defines.h:
+
+../../inet/src/inet/features.h:
+
+../../inet/src/inet/common/Compat.h:
+
+../../inet/src/inet/common/InitStages.h:
+
+../../inet/src/inet/networklayer/contract/IInterfaceTable.h:
+
+../../inet/src/inet/networklayer/common/InterfaceEntry.h:
+
+../../inet/src/inet/common/packet/tag/TagSet.h:
+
+../../inet/src/inet/common/Simsignals.h:
+
+../../inet/src/inet/common/Simsignals_m.h:
+
+../../inet/src/inet/common/INETDefs_m.h:
+
+../../inet/src/inet/linklayer/common/MacAddress.h:
+
+../../inet/src/inet/networklayer/common/InterfaceToken.h:
+
+../../inet/src/inet/networklayer/common/L3Address.h:
+
+../../inet/src/inet/networklayer/common/ModuleIdAddress.h:
+
+../../inet/src/inet/networklayer/common/ModulePathAddress.h:
+
+../../inet/src/inet/networklayer/contract/clns/ClnsAddress.h:
+
+../../inet/src/inet/networklayer/contract/ipv4/Ipv4Address.h:
+
+../../inet/src/inet/networklayer/contract/ipv6/Ipv6Address.h:
+
+../../inet/src/inet/common/StringFormat.h:
+
+../../inet/src/inet/common/geometry/common/CanvasProjection.h:
+
+../../inet/src/inet/common/geometry/common/RotationMatrix.h:
+
+../../inet/src/inet/common/geometry/common/Coord.h:
+
+../../inet/src/inet/common/INETMath.h:
+
+../../inet/src/inet/common/geometry/common/EulerAngles.h:
+
+../../inet/src/inet/common/Units.h:
+
+../../inet/src/inet/common/geometry/common/Quaternion.h:
+
+../../inet/src/inet/mobility/contract/IMobility.h:
+
+veins_inet/veins_inet.h:
+
+/home/leonardo/artery/extern/veins/src/veins/veins.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIScenarioManager.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/utils/Coord.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/utils/FWMath.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/modules/BaseWorldUtility.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/connectionManager/BaseConnectionManager.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/utils/AntennaPosition.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/connectionManager/NicEntry.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/utils/Heading.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/utility/HasLogProxy.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/utils/FindModule.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/obstacle/ObstacleControl.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/obstacle/Obstacle.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/world/annotations/AnnotationManager.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/utility/BBoxLookup.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/obstacle/VehicleObstacleControl.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/utils/Move.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/obstacle/MobileHostObstacle.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIBuffer.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIConstants.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIColor.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIConnection.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCICoord.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/VehicleSignal.h:
+
+/home/leonardo/artery/extern/veins/src/veins/base/utils/EnumBitset.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/mobility/traci/TraCICommandInterface.h:
+
+/home/leonardo/artery/extern/veins/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h:
+
+../../inet/src/inet/common/geometry/common/GeographicCoordinateSystem.h:
